82b12a475d219889dd7852a6b7a008aa30849df6
unknown <datndhe160186@fpt.edu.vn>
2023-11-23 15:32:57 +0700

    modified and add login

--- group1/src/App.js
+++ group1/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Sign_In from './Screen/Sign_In';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { ToastContainer } from 'react-toastify';
+import Sign_Up from './Screen/Sign_Up';
 function App() {
   return (
     <div className="App">
@@ -10,7 +13,10 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home/>}></Route>
+          <Route path='/sign_in' element={<Sign_In/>}></Route>
+          <Route path='sign_up' element={<Sign_Up/>}></Route>
         </Routes>
+        <ToastContainer/>
       </BrowserRouter>
 
     </div>
---
+++ group1/src/Screen/Sign_In.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormControl,
+  FormGroup,
+  Row,
+} from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+const Sign_In = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate();
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
+  const ProceedLogin = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      console.log(email, password);
+      axios
+        .post("https://api.realworld.io/api/users/login", {
+          user: {
+            email: email,
+            password: password,
+          },
+        })
+        .then((res) => {
+          const user = res.data.user;
+          console.log(res);
+          localStorage.setItem("user", JSON.stringify(user));
+          localStorage.setItem("token", user.token);
+          toast.success("Success");
+          nav("/");
+        })
+        .catch((err) => {
+          toast.error("Login Failed due to :" + err.message);
+        });
+    }
+  };
+  const validate = () => {
+    let result = true;
+    if (email === "" || email === null) {
+      result = false;
+      toast.warning("Please Enter email");
+    }
+    if (password === "" || password === null) {
+      result = false;
+      toast.warning("Please Enter Password");
+    }
+    return result;
+  };
+  return (
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <div>
+            <h2 className="text-center">Sign In</h2>
+            <p className="text-center">
+              <Link to="/sign_up" className="text-success">
+                Need an account?
+              </Link>
+            </p>
+            <Form onSubmit={ProceedLogin}>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <FormControl
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Button type="submit" className="btn btn-success">
+                  Sign In
+                </Button>
+              </FormGroup>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Sign_In;
