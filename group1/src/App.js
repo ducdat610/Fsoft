4dd38b013fec3bae0b17ad21728ffc6677ee24f7
unknown <datndhe160186@fpt.edu.vn>
2023-11-28 15:34:35 +0700

    getting

--- group1/src/App.js
+++ group1/src/App.js
@@ -6,6 +6,7 @@ import Sign_In from './Screen/Sign_In';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { ToastContainer } from 'react-toastify';
 import Sign_Up from './Screen/Sign_Up';
+import Setting from './Screen/Setting';
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Home/>}></Route>
           <Route path='/sign_in' element={<Sign_In/>}></Route>
           <Route path='sign_up' element={<Sign_Up/>}></Route>
+          <Route path='setting' element={<Setting/>}></Route>
         </Routes>
         <ToastContainer/>
       </BrowserRouter>
